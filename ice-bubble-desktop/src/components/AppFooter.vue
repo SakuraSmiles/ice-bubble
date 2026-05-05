8a0b7e436cfca77976c7225ad4206add5e478953
SakuraSmiles <306596878@qq.com>
2026-05-05 13:35:57 +0800

    fix: AppFooter合并版本号显示，只保留一个version

--- ice-bubble-desktop/src/components/AppFooter.vue
+++ ice-bubble-desktop/src/components/AppFooter.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import pkg from '../../package.json';
 import { APP_VERSION } from '../version';
-const version = pkg.version;
 </script>
 
 <template>
   <footer class="copyright">
     <div>IceBubble © 2026 · SakuraSmiles</div>
-    <div>desktop version : {{ version }}</div>
-    <div>app version : {{ APP_VERSION }}</div>
+    <div>version : {{ APP_VERSION }}</div>
   </footer>
 </template>
 
